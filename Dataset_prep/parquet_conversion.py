4bb95e1c7bf18ff93ce2d21585df8e3ebef810a3
fickya1987 <89722540+fickya1987@users.noreply.github.com>
2025-02-16 10:36:41 +0700

    Update parquet_conversion.py

--- Dataset_prep/parquet_conversion.py
+++ Dataset_prep/parquet_conversion.py
@@ -12,7 +12,7 @@ dtype = {
 }
 
 # Load CSV data with specified data types
-df = pd.read_csv('airport.csv', dtype=dtype)
+df = pd.read_csv('seaport_01.csv', dtype=dtype)
 
 # Save as Parquet using the specified engine
 df.to_parquet('airport.parquet', engine=parquet_engine)
